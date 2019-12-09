1a98b5bb9f8b456ccfd929473eea61ef9fb7076c
Naman Attri <namanattri5@gmail.com>
2019-12-09 09:01:00 +0530

    Send one SMS to multiple phone numbers

---
+++ src/examples/send-one-to-many.ts
@@ -0,0 +1,3 @@
+import { sendSmsToMultipleNumbers } from '../index';
+
+sendSmsToMultipleNumbers('YOUR-API-KEY', [/** Array of phone numbers */], 'SENDER-CODE-6-LETTERS', 'Message matching the template');
--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,14 @@ export const sendSms = (apikey: string, phoneNumber: number, sender: string, mes
 
   return axios.post('https://api.textlocal.in/send/', params);
 };
+
+export const sendSmsToMultipleNumbers = (apikey: string, phoneNumbers: number[], sender: string, message: string) => {
+  const params = new URLSearchParams();
+
+  params.append('apikey', apikey);
+  params.append('numbers', `${phoneNumbers.join(',')}`);
+  params.append('sender', sender);
+  params.append('message', message);
+
+  return axios.post('https://api.textlocal.in/send/', params);
+};
